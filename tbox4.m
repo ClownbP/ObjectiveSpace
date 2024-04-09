dd1120a8814717af6f13b1beaa35fe9f8a915d75
Kaushik Kashyap <138221951+ClownbP@users.noreply.github.com>
2024-04-10 03:34:16 +0530

    New

--- tbox4.m
+++ tbox4.m
@@ -1,4 +1,4 @@
 sD=som_read_data('Data3.data');
-sM=som_make(sD);
+som_gui([sD]);
 som_show(sM,'umat', 'all', 'comp', 1:3,'empty', 'Labels', 'norm', 'd');
 som_show_add('label', SM, 'subplot', 10);
--- tryGui.m
+++ tryGui.m
@@ -1,4 +1,4 @@
-sD=som_read_data('iris1.data');
-som_gui([sD]);
+sD=som_read_data('Data2.data');
+sM=som_make(sD);
 som_show(sM,'umat', 'all', 'comp', 1:3,'empty', 'Labels', 'norm', 'd');
 som_show_add('label', SM, 'subplot', 10);
